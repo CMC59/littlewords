3e54d3ac245ca0e42274e370d307cd72f188e6ce
EL AINE El Mehdi <ainemehdi6@gmail.com>
2022-11-30 17:02:23 +0100

    Version (verified)

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
