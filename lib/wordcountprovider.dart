e4b616d677eea9780e231f0084a89ec94b701abe
CMC59 <philippeplaia@gmail.com>
2022-12-07 16:20:45 +0100

    DbHelper (Philippe)

---
+++ lib/wordcountprovider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'DbHelper.dart';
+
+final wordCountProvider = FutureProvider<int>((ref) async {
+  return DbHelper.instance.countWords();
+});
