92c5b7c8264fefb0fb5e0835b2a716a1192a7351
EL AINE El Mehdi <ainemehdi6@gmail.com>
2022-12-14 13:58:15 +0100

    Gestion du username

---
+++ lib/shared_pref.provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPrefProvider = FutureProvider<SharedPreferences>((ref){
+  return SharedPreferences.getInstance();
+});
+
+final usernameProvider = FutureProvider((ref) async {
+  SharedPreferences.getInstance()
+});
